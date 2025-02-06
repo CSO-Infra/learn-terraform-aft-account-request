474dbed8919d008f30206d24ae7aee688d7c8918
marklehmann26 <mark@mksvr01.com>
2025-02-06 18:42:14 +0000

    new account

--- terraform/sandbox.tf
+++ terraform/sandbox.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "cpes-infra+testing@centricsoftware.onmicrosoft.com"
+    AccountEmail              = "cpes-infra+testing1@centricsoftware.onmicrosoft.com"
     AccountName               = "sandbox-testing"
     ManagedOrganizationalUnit = "controller"
-    SSOUserEmail              = "cpes-infra+testing@centricsoftware.onmicrosoft.com"
+    SSOUserEmail              = "cpes-infra+testing1@centricsoftware.onmicrosoft.com"
     SSOUserFirstName          = "controller"
     SSOUserLastName           = "AFT"
   }
