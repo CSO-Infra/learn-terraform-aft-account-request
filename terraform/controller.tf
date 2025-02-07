7c4f01f394e7fef9a928533bf26fc1aef4421195
marklehmann26 <mark@mksvr01.com>
2025-02-07 08:50:57 +0000

    account edit

--- terraform/controller.tf
+++ terraform/controller.tf
@@ -23,34 +23,5 @@ module "sandbox" {
     group = "non-prod"
   }
 
-  account_customizations_name = "sandbox"
-}
-
-
-module "Controller" {
-  source = "./modules/aft-account-request"
-
-  control_tower_parameters = {
-    AccountEmail              = "cpes-infra+mark-lehmann@centricsoftware.onmicrosoft.com"
-    AccountName               = "controller-01"
-    ManagedOrganizationalUnit = "controller (pp-ghnkmog5hfpwc)"
-    SSOUserEmail              = "cpes-infra+mark-lehmann@centricsoftware.onmicrosoft.com"
-    SSOUserFirstName          = "controller"
-    SSOUserLastName           = "AFT"
-  }
-
-  account_tags = {
-    "Learn Tutorial" = "AFT"
-  }
-
-  change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
-  }
-
-  custom_fields = {
-    group = "non-prod"
-  }
-
-  account_customizations_name = "controller-01"
+  account_customizations_name = "sndbox-new"
 }
